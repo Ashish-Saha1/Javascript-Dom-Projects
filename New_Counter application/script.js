977f16a9dcf27754fb07ca5f83ffa77ba75e3aae
ashish.saha <rubelsaha1@gmail.com>
2023-12-05 17:48:46 +0600

    Counter application almost complete from office

--- New_Counter application/script.js
+++ New_Counter application/script.js
@@ -6,8 +6,43 @@ window.onload = function(){
 }
 
  function main(){
-   
+   let result = document.querySelector('.result-show');
+   let incrementInput = document.getElementById('increment-input');
+   let decrementInput = document.getElementById('decrement-input');
+   let incrementBtn = document.querySelector('.increment-btn');
+   let decrementBtn = document.querySelector('.decrement-btn');
     
+
+    //console.log(parseInt(result.innerHTML) + 15);
+    //console.log(parseInt(result.innerHTML) + parseInt(incrementInput.value));
+    
+    incrementBtn.addEventListener('click', function(){       
+        if(incrementInput.value === ""){
+            alert('First input any number')
+        }else{
+            result.innerHTML = parseInt(incrementInput.value) + parseInt(result.innerHTML)
+        }
+        
+    })
+
+
+    decrementBtn.addEventListener('click', function(){       
+        if(decrementInput.value === ""){
+            alert('First input any number')
+        }else{
+            if(parseInt(result.innerHTML) - parseInt(decrementInput.value) < 0){
+                result.innerHTML = 0;
+            }else{
+                result.innerHTML = parseInt(result.innerHTML) - parseInt(decrementInput.value)
+            }
+        }
+        
+        
+            
+       
+        
+    })
+
  }
 
 
