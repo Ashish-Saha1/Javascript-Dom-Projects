9502b336704c4f1dc1d69b95a73845ef6a3083e0
ashish.saha <rubelsaha1@gmail.com>
2023-12-07 18:13:06 +0600

    Converter project HTML done javascript ongoing from offic

--- New_Multiple converter/script.js
+++ New_Multiple converter/script.js
@@ -8,14 +8,59 @@ window.onload = function(){
 }
 
  function main(){
-   
-         
+    const options = {
+        length:{
+            name : "Length"
+            },
+
+        time:{
+            name: "Time"
+            },
+
+        weight: {
+            name: "Weight"
+            },
+
+        dataTransferRate:{
+            name: "Data Transfer Rate"
+            }
+    }
+       
+    
+    let selectCategoryFirst = document.getElementById('category-select');
+
+
+    addOptions(selectCategoryFirst, options);
        
     }
 
 
+  
+    // function addOptions(parent, option){
+    //     let optionsNameArr = Object.values(option);
+    //     let optionsValueArr = Object.keys(option);
+
+    //     for(let optionName of optionsNameArr){
+    //         let createOptionDomElement = document.createElement('option');
+    //         createOptionDomElement.innerHTML = optionName.name;
+    //         parent.appendChild(createOptionDomElement)
+    //         }
+    //         //createOptionDomElement.setAttribute('values', Object.keys(option))  
+          
+    //     }
+    
    
 
+    function addOptions(parent, option){
+        let optionsNameArr = Object.values(option);
+        let optionsValueArr = Object.keys(option);
+
+        optionsNameArr.map(value, index, arr){
+            console.log(value);
+            
+        }
+          
+        }
 
 
 
