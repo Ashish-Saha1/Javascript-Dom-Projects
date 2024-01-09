5292201a33d92c36c70c1a7cdc3d7fd70c95a67d
Ashish Saha <rubelsaha1@gmail.com>
2024-01-09 22:21:31 +0600

    Media quary add on Css sheet, addoptions functions done & units add in comverter object

--- New_Multiple converter -again/script.js
+++ New_Multiple converter -again/script.js
@@ -6,21 +6,67 @@ window.onload = function(){
 const converter = {
     area: {
         name: "Area",
+        units: {
+            squareKm: 'Square Kilometer',
+            squareM: 'Square Meter',
+            squareMile: 'Square MIle',
+            squareYard: "Square Yard",
+            squareFoot: "Square Foot"
+        }
     },
+
     mass: {
         name: "Mass",
+        units: {
+            tonne: "Tonne",
+            kilogram: "Kilogram",
+            gram: "Gram",
+            milligram: "Milligram",
+            microgram: "Microgram",
+            pound: "Pound",
+            ounce: "Ounce", 
+        }
     },
+
     length: {
         name: "Length",
+        units:  {
+            kilometer: "Kilometer",
+            meter: "Meter",
+            centimeter: "Centimeter",
+            millimeter: "Millimeter",
+            micrometer: "Micrometer",
+            nenometer: "Nenometer",
+            mile: "Mile",
+            yard: "Yard",
+            foot: "Foot",
+            inch: "Inch",
+            nauticalMile: "NauticalMile"
+        }
     },
-    volume: {
-        name: "Volume",
-    },
+
     time: {
         name: "Time",
+        units:  {
+            nanosecond: "Nanosecond",
+            microsecond: "Microsecond",
+            millisecond: "Millisecond",
+            second: "Second",
+            minute: "Minute",
+            hour: "Hour",
+           
+        }
     },
+
     dataTransferRate: {
-        name: "Data Transfer Rate"
+        name: "Data Transfer Rate",
+        units:  {
+            bitePerS: "Bit per second",
+            kilobitePerS: "Kilobite per second",
+            megabitePerS: "Megabyte per second",
+            gigabitePerS: "Gigabyte per second",
+            terabitePerS: "Terabyte per second", 
+        }
     }
 }
 
@@ -28,41 +74,40 @@ const converter = {
 
 
 function main(){
- 
-    let x = Object.keys(converter)
-
-    
-
-    for(let i=0; i<x.length; i++){
-        console.log(converter[x[i]].name);
-        console.log(x[i]);
-        
-    }
-    
-
-}
-
-
-function addCategory(){
     let inputCategory = document.getElementById('category-select');
-   
-    let x = Object.keys(converter)
-    let converterName = Object.values(converter)
-    for(let i = 0; i<converterName.length; i++){
-       let createDivElement  = document.createElement('option');
-        createDivElement.innerHTML = converterName[i].name;
-        createDivElement.value = x[i];
-        
-        
+        removeAllChild(inputCategory)
+    let converterKeys = Object.keys(converter).sort();
+        converterKeys.forEach((item)=>{
+            addOptionsInCategory(inputCategory, {value: item, text: converter[item].name})
+        })
 
-        inputCategory.appendChild(createDivElement)
-    }
-
-   
-    
-    
-    
 }
+
+
+function addOptionsInCategory(parent, option){
+    let createOptions = document.createElement('option');
+    createOptions.setAttribute('value', option.value);
+    createOptions.innerText = option.text;
+
+    parent.appendChild(createOptions);
+
+
+}
+
+
+function removeAllChild(parent){    
+    while(parent.firstChild){
+        //Both way you have executed to remove child element
+            //parent.removeChild(parent.firstChild)
+            parent.firstChild.remove()
+    }
+}
+
+   
+    
+    
+    
+
 
 
 
